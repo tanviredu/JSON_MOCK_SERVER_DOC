203ad76dc539ced1a41b53d0d8dde776c3c0739e
tanviredu <ornobtanvir.git@gmail.com>
2020-11-11 20:07:17 +0600

    added

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "mongourl": "mongodb+srv://trahman123456:trahman123456@cluster0.v1x5w.mongodb.net/todo?retryWrites=true&w=majority",
+    "redisurl":"redis-12777.c232.us-east-1-2.ec2.cloud.redislabs.com:12777",
+    "port": process.env.PORT || 3000,
+    "secrets": process.env.JWT || "secret",
+    "expiretime": 24 * 60 * 10
+}
